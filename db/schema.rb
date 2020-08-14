3bc332e12947b526c8bf43b9663e65f0cde12993
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2020-08-14 05:04:09 +0000

    交通機関ページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_14_033626) do
 
   create_table "fees", force: :cascade do |t|
